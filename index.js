a4ce6ea763fd96b2cc6361174c6cee73cb1e7fb6
AdhanTheFullStack <adhan.benites@estudante.ifms.edu.br>
2026-06-25 13:48:48 -0400

    Refactor API endpoints for pessoa management

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 const express = require('express');
-const app = express();
 const db = require('./db');
+const app = express();
+const cors = require('cors')
 app.use(express.json());
 
-app.use('/',express.static('static'));
+app.use('/', express.static('static'));
 
-app.use(cors( {origin: 'http://localhost:5173'} ));
+app.use(cors({ origin:'http://localhost:5173'}));
 
-app.get('/api/pessoa', async(req,res)=>{
-    const results = await db.listarCliente();
+app.get('/api/pessoa', async (req, res) => {
+    const results = await db.listPessoa();
     res.json(results);
 });
 
-app.get('/api/pessoa/:id', async(req,res)=>{
-    const results = await db.buscarCliente(req.params.id);
+//{"id": 1,"nome": "Adhan Felipe","email": "adhan.benbites@ifms.edu.br"}
+
+app.post('/api/pessoa', async (req, res)=>{
+    let param = req.body;
+    const data = await db.incluiPessoa(param.nome, param.email);
+    res.json(data);
+});
+app.put('/api/pessoa', async (req   , res)=>{
+    let param = req.body;
+    const data = await db.atualizarPessoa(param.id, param.nome, param.email);
+    res.json(data);
+});
+app.delete('/api/pessoa/:id', async (req,res) => {
+    const results = db.removePessoa(req.params.id);
     res.json(results);
 });
 
-app.post('api/pessoa', async (req, res)=>{
-    const data = req.body;
-    const results = await db.incluirCliente(data.nome, data.email, data.cpf, data.celular, data.pix, data.genero, data.nascimento);
-    res.json(results);
+app.get('/api', async (req, resp)=> {
+    resp.send('Olá Mundo');
 });
-
-app.put('api/pessoa', async (req, res)=> {
-    const data= req.body;
-    const results = await db.atualizarCliente(data.id ,data.nome, data.email, data.cpf, data.celular, data.pix, data.genero, data.nascimento);
-    res.json(results);
-});
-
-app.delete('api/pessoa/:id', async (req, res)=> {
-    const results = await db.removeCliente(req.params.id);
-    res.json(results);
-});
-
 app.listen(3000);
+console.log('Servidor rodando na porta 3000');
